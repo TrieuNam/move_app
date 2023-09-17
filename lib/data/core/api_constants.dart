25cb81374786bdcf82067e0a640308bb7c5b7de7
trieunam <trieuphuongnam85@gmail.com>
2023-09-16 23:42:41 -0700

    commit code get APi trending using HTTP.
    Need refactor get API treding

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,6 @@
+class ApiConstants{
+  ApiConstants._();
+  static const String BASE_URL="https://api.themoviedb.org/3/";
+  static const String API_KEY="1ba70a9954ad66886d52b23e02d00601";
+  static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+}
