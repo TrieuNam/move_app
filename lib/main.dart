52528d63e31e3851798f64c1ab0f20949e017e65
trieunam <trieuphuongnam85@gmail.com>
2023-09-17 06:34:36 -0700

    commit code get_it.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
+import 'dart:async';
+
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:move_app/data/core/api_client.dart';
-import 'package:move_app/data/data_sources/movie_remote_data_source.dart';
-import 'package:move_app/data/repositories/movie_repository_impl.dart';
 import 'package:move_app/domain/entities/app_error.dart';
 import 'package:move_app/domain/entities/movies_entity.dart';
 import 'package:move_app/domain/entities/no_params.dart';
-import 'package:move_app/domain/repositories/movie_repository.dart';
 import 'package:move_app/domain/usecases/get_trending.dart';
+import 'package:pedantic/pedantic.dart';
+import 'di/get_it.dart' as getIt;
 
-void main() async {
-  ApiClient apiClient = ApiClient(Client());
-  MovieRemoteDataSource dataSource = MovieRemoteDataSourceImpl(apiClient);
-  MovieRepository movieRepository = MovieRepositoryImpl(dataSource);
-  GetTrending getTrending = GetTrending(movieRepository);
+Future<void> main() async {
+  unawaited(getIt.init());
+  GetTrending getTrending = getIt.getItInstance<GetTrending>();
   final Either<AppError, List<MovieEntity>?> eitherResponse =
       await getTrending(NoParams());
   eitherResponse.fold((l) {
@@ -39,9 +36,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  Container(),
+      home: Container(),
     );
   }
 }
-
-
